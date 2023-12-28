971cf2899edbb8ae6fb1c93be94781cb28be0847
MajorAmari <major.amari@gmail.com>
2023-12-28 03:34:17 +0200

    Add commands and context menus to AstrolabeClient

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -1,12 +1,17 @@
 import { red } from "colorette";
-import { type ClientOptions, Client } from "discord.js";
+import { type ClientOptions, Client, Collection } from "discord.js";
 
+import { type Command } from "./command";
 import { registerEvents } from "./event";
+import commandsFiles from "@commands";
 import events from "@events";
 import { configs } from "@modules/config";
 import { log } from "@modules/logger";
 
 export class AstrolabeClient extends Client {
+  readonly commands: Collection<string, Command> = this.getCommands();
+  readonly contextMenu: Collection<string, Command> = this.getContextMenus();
+
   constructor(options: ClientOptions) {
     super(options);
 
@@ -19,5 +24,30 @@ export class AstrolabeClient extends Client {
       );
       process.exit(1);
     });
+  }
+
+  // ? Helpers
+  // * Get all commands from the commands folder
+  private getCommands(): Collection<string, Command> {
+    const mappedCommand = commandsFiles.map(({ commands }) => commands).flat();
+    return new Collection<string, Command>(
+      mappedCommand.map((data) => {
+        log.command(data.cmd.meta.name);
+        return [data.cmd.meta.name, data];
+      }),
+    );
+  }
+
+  // * Get all context menus from the commands folder
+  private getContextMenus(): Collection<string, Command> {
+    const mappedCommand = commandsFiles.map(({ commands }) => commands).flat();
+    return new Collection<string, Command>(
+      mappedCommand
+        .filter((data) => data.ctx?.meta)
+        .map((data) => {
+          log.command(data.ctx!.meta.name);
+          return [data.ctx!.meta.name, data];
+        }),
+    );
   }
 }
